060db64a7144ba80ba2a7e964187385cd3e8a6bc
myst <johndaveaquino35@gmail.com>
2024-01-27 14:29:04 +0800

    fixed bugs

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
-import Image from "next/image";
 import { space } from "postcss/lib/list";
 const NavBar = () => {
   const { data: session, status } = useSession();
@@ -10,15 +9,6 @@ const NavBar = () => {
         <p className="text-white font-bold">User Management System</p>
         {session && (
           <div className="flex items-center sm:space-x-2 justify-end right-10 absolute">
-            {/* <Image
-              onClick={signOut}
-              className="rounded-full cursor-pointer"
-              src={session.user.image}
-              height="30"
-              width="30"
-              layout="fixed"
-              title="Click to logout"
-            ></Image> */}
             <p className="text-white cursor-pointer" onClick={signOut}>
               {session?.user.name}
             </p>
